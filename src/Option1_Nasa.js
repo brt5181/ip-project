e48385c330978a22b4f04cd89d7c7d1d0a436fcc
Bee <brt5181@psu.edu>
2022-02-06 18:33:25 -0500

    Group 1 need to fix the render

--- src/Option1_Nasa.js
+++ src/Option1_Nasa.js
@@ -1,14 +1,40 @@
 // dependencies / things imported
+// API KEY    dk7GLMwNo1M9bWTRJaEZNaGFZdDzxfNYpJSbBBiE
 import { LitElement, html } from 'lit';
 import '@lrnwebcomponents/accent-card/accent-card.js';
 
 export class imageSearch extends LitElement {
+  constructor() {
+    super();
+    this.nasaUrl = 'https://images-api.nasa.gov/search?api_key=';
+    this.apikey = 'dk7GLMwNo1M9bWTRJaEZNaGFZdDzxfNYpJSbBBiE';
+    this.searchName = 'Aliens';
+    this.media_type = 'image';
+    this.image = [];
+    this.title = [];
+    this.discription = [];
+    this.secondary_creator = [];
+  }
   // static properties = {
 
   // };
-  // constructor{
-
-  // }
+  async getNasaData() {
+    return fetch(this.nasaUrl + this.apikey + this.searchName)
+      .then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        }
+        return false;
+      })
+      .then(data => {
+        console.log(data);
+        this.image = data.href;
+        this.title = data.title;
+        this.description = data.description;
+        this.secondary_creator = data.secondary_creator;
+        return data;
+      });
+  }
 
   render() {
     return html` <accent-card
@@ -17,10 +43,9 @@ export class imageSearch extends LitElement {
       horizontal
       style="max-width:600px;"
     >
-      <div slot="heading">Ut Integer Massa Nulla Sed</div>
-      <div slot="content">
-        Libero elit sed consequat. Ut nisi nulla sapien id a ultricies.
-      </div>
+      <div slot="heading">${this.title}</div>
+      <div slot="content">${this.description}</div>
+      <div slot="Secondary">${this.secondary_creator}</div>
     </accent-card>`;
   }
 }
